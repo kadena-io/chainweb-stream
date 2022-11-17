31aaeaa40ae30e6b64d5c955b99a28bbe3a7b521
Takadenoshi <tasos@kadena.io>
2022-11-17 15:21:25 +0100

    generic withRetries() wrapper to retry async functions

---
+++ src/sse/__tests__/with-retries.js
@@ -0,0 +1,92 @@
+import { expect, test, jest, beforeEach, afterEach, } from '@jest/globals';
+import { withRetries } from '../utils.js';
+
+function rejectThenResolve(result, fails=0, failErrorMessage = 'Simulated Failure') {
+  return async () => {
+    if (fails <= 0) {
+      return result;
+    } else {
+      fails--;
+      throw new Error(failErrorMessage);
+    }
+  }
+}
+
+
+test('Expect withRetries to return resolved promise (immediate success)', async () => {
+  const expected = { success: 1 };
+  const fn = rejectThenResolve(expected, 0);
+  const actual = await withRetries(fn);
+  expect(actual).toBe(expected); // intentionally not using toEqual
+});
+
+test('Expect withRetries to return resolved promise (one failure)', async () => {
+  const expected = { success: 1 };
+  const fn = rejectThenResolve(expected, 1);
+  const actual = await withRetries(fn);
+  expect(expected).toBe(actual);
+});
+
+test('Expect withRetries with timeoutFn to return resolved promise (two failures)', async () => {
+  const expected = { success: 1 };
+  const fn = rejectThenResolve(expected, 2);
+  const actual = await withRetries(fn, { timeoutFn: n => Math.pow(n, 2) * 100 });
+  expect(expected).toBe(actual); // intentionally not using toEqual
+});
+
+test('Expect withRetries with maxAttempts=2 and timeoutFn to succeed', async () => {
+  const expected = { success: 1 };
+  const fn = rejectThenResolve(expected, 2);
+  const actual = await withRetries(fn, { maxAttempts: 2, timeoutFn: n => Math.pow(n, 2) * 100 });
+  expect(expected).toBe(actual); // intentionally not using toEqual
+});
+
+test('Expect withRetries with maxAttempts=1 and timeoutFn to fail after two failures', async () => {
+  const expected = { success: 1 };
+  const fn = rejectThenResolve(expected, 2);
+  try {
+    await withRetries(fn, { maxAttempts: 1, timeoutFn: n => Math.pow(n, 2) * 100 });
+  } catch(e) {
+    expect(e.message).toBe('Simulated Failure');
+  }
+});
+
+test('Expect withRetries with non numeric maxAttempts to fail', async () => {
+  const expected = { success: 1 };
+  const fn = rejectThenResolve(expected, 0);
+  try {
+    await withRetries(fn, { maxAttempts: 'a' });
+  } catch(e) {
+    expect(e.message).toBe('withRetries: expected positive maxAttempts, received a')
+  }
+});
+
+test('Expect withRetries with maxAttempts=0 to fail', async () => {
+  const expected = { success: 1 };
+  const fn = rejectThenResolve(expected, 0);
+  try {
+    await withRetries(fn, { maxAttempts: 0 });
+  } catch(e) {
+    expect(e.message).toBe('withRetries: expected positive maxAttempts, received 0');
+  }
+});
+
+test('Expect withRetries with negative maxAttempts to fail', async () => {
+  const expected = { success: 1 };
+  const fn = rejectThenResolve(expected, 0);
+  try {
+    await withRetries(fn, { maxAttempts: -1 });
+  } catch(e) {
+    expect(e.message).toBe('withRetries: expected positive maxAttempts, received -1');
+  }
+});
+
+test('Expect withRetries with an incorrect timeoutFn fail', async () => {
+  const expected = { success: 1 };
+  const fn = rejectThenResolve(expected, 0);
+  try {
+    await withRetries(fn, { timeoutFn: () => -1 });
+  } catch(e) {
+    expect(e.message).toBe('withRetries: timeoutFn expected to return positive number, received -1');
+  }
+});
--- src/sse/utils.js
+++ src/sse/utils.js
@@ -1,5 +1,6 @@
 import filter from 'lodash/filter.js';
 import flatten from 'lodash/flatten.js';
+import defaults from 'lodash/defaults.js';
 
 export function sortEvents(completedResults, newestToOldest = false) {
   completedResults.sort((event1, event2) =>
@@ -43,4 +44,53 @@ export function summarizeChainwebCut(data) {
     return out;
   }, [Infinity, -Infinity]);
   return { instance, min, max, height };
+}
+
+const DEFAULT_RETRY_OPTIONS = {
+  maxAttempts: 5,
+  timeoutFn: n => Math.pow(n, 2) * 1000,
+};
+
+export async function withRetries(fn, options) {
+  const { maxAttempts, timeoutFn } = defaults(options, DEFAULT_RETRY_OPTIONS);
+  let attempts = 1;
+
+  // maxAttempts > 0
+  if (!isPositiveNumber(maxAttempts)) {
+    throw new Error(`withRetries: expected positive maxAttempts, received ${maxAttempts}`);
+  }
+
+  // test first timeoutFn value
+  // so as to fail deterministically if that is incorrect, instead of rely on fn() failing to trigger it
+  const firstTimeout = timeoutFn(1);
+  if (!isNonNegativeNumber(firstTimeout)) {
+    throw new Error(`withRetries: timeoutFn expected to return positive number, received ${firstTimeout}`);
+  }
+
+  while(true) {
+    try {
+      return await fn();
+    } catch(e) {
+      if (attempts > maxAttempts) {
+        throw e;
+      }
+      const timeout = timeoutFn(attempts);
+      // timeout >= 0
+      if (!isNonNegativeNumber(timeout)) {
+        console.error(`Skipping retries: the timeoutFn passed to withRetries did not return a positive number`);
+        throw e;
+      }
+      console.warn(`withRetries: ${process.env.ENV} Retryable error (${attempts}/${maxAttempts}; retry in ${timeout}ms): ${e.message}`);
+      await sleep(timeout);
+      attempts++;
+    }
+  }
+}
+
+function isPositiveNumber(num) {
+  return Number.isFinite(num) && num > 0;
+}
+
+function isNonNegativeNumber(num) {
+  return Number.isFinite(num) && num >= 0;
 }
