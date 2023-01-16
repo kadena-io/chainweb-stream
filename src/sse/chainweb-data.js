09188551cde9dc2c9279db5c5eb48ab3a92b3e04
Takadenoshi <tasos@kadena.io>
2023-01-16 17:47:46 +0100

    cw-data support cw-next token

--- src/sse/chainweb-data.js
+++ src/sse/chainweb-data.js
@@ -6,34 +6,36 @@ import { filterBlackListItems, sortEvents } from './utils.js';
 
 const { dataHost } = config;
 
-export async function getChainwebDataEvents(name, minHeight, limit = 50, offset = 0, logger = console) {
+export async function getChainwebDataEvents(endpoint, name, minHeight, limit = 50, next = undefined, logger = console) {
   const params = new URLSearchParams({
     name,
     limit,
-    ...(offset ? { offset } : null),
     ...(minHeight ? { minheight: minHeight } : null),
+    ...(next ? { next } : null),
   });
-  const url = `${dataHost}/txs/events?${params}`;
+  const url = `${dataHost}/txs/${endpoint}?${params}`;
 
   const timeStart = Date.now();
   const rawRes = await fetchWithRetry(url, { logger });
   const elapsed = Date.now() - timeStart;
 
+  const nextNext = rawRes.headers.get('chainweb-next');
+
   const response = await getResponse(rawRes);
   if (typeof response === "string") {
-    logger.error(`Got CW-Data "${name}" events error limit=${limit} offset=${offset} response="${response}" in ${elapsed} ms`);
+    logger.error(`Got CW-Data "${name}" events error limit=${limit} fromNext=${next} nextNext=${nextNext} response="${response}" in ${elapsed} ms`);
     return []
   }
-  logger.verbose(`Got CW-Data "${name}" events limit=${limit} offset=${offset} responseLength=${response.length} in ${elapsed} ms`);
-  return response;
+  logger.verbose(`Got CW-Data "${name}" events limit=${limit} fromNext=${next} nextNext=${nextNext} responseLength=${response.length} in ${elapsed} ms`);
+
+  return { response, next: nextNext };
 }
 
 const syncEventsFromChainwebDataDefaults = {
   limit: 50,
   threads: 4,
-  newestToOldest: false,
   moduleHashBlacklist: [],
-  totalLimit: 0, // DEBUG for quicker results
+  totalLimit: Infinity,
 }
 
 export async function syncEventsFromChainwebData(opts, logger=console) {
@@ -41,38 +43,33 @@ export async function syncEventsFromChainwebData(opts, logger=console) {
     filter,
     limit,
     threads,
-    newestToOldest,
     moduleHashBlacklist,
     totalLimit,
     minHeight,
     callback,
   } = defaults(opts, syncEventsFromChainwebDataDefaults);
 
-  let offset = 0;
-  let resultPromises = []
-  let completedResults = [];
-  let continueSync = true;
-
+  const completedResults = [];
   logger.debug(`syncEventsFromChainwebData minHeight=${minHeight}`);
 
+  const resultPromises = [];
   // TODO slow start with threads=1 if minHeight is set
   // most of the times there won't be updates, 1 thread enough to check
-  while (continueSync) {
-    let batchPromises = [];
-    for (let i = 0; i < threads; i++) {
-      // TODO slow start with threads=1 when checking for updates
-      const fetchPromise = getChainwebDataEvents(filter, minHeight, limit, offset, logger);
-      batchPromises.push(fetchPromise);
-      fetchPromise.then(events => resultPromises.push(callback(filterBlackListItems(events, moduleHashBlacklist))));
-      offset = offset + limit;
+  let _next;
+  while (completedResults.length < totalLimit ) {
+    const { response, next } = await getChainwebDataEvents('events', filter, minHeight, limit, _next, logger);
+    const data = filterBlackListItems(response, moduleHashBlacklist);
+    try {
+      resultPromises.push(callback(data));
+    } catch(e) {
+      logger.warn("Callback error:", e);
     }
-    completedResults.push(...await Promise.all(batchPromises));
-    // logger.debug('Got', completedResults.reduce((num, arr) => num + arr.length, 0));
-    // once a batch comes back empty, we're caught up
-    continueSync = completedResults.every((v) => v.length >= limit) &&
-      ( totalLimit === 0  ||
-        completedResults.reduce((total, results) => total + results.length, 0) <= totalLimit
-      );
+    completedResults.push(...data);
+    if (completedResults.length > totalLimit || !next)
+      break;
+    _next = next;
+    console.log('next', next);
+    debugger;
   }
 
   try {
@@ -80,14 +77,13 @@ export async function syncEventsFromChainwebData(opts, logger=console) {
   } catch(e) {
   }
 
-  completedResults = filterBlackListItems(completedResults, moduleHashBlacklist);
-
-  // sortEvents(completedResults, newestToOldest);
-
   logger.verbose('sync fetch finished. awaiting result processors', completedResults.length);
-  await Promise.all(resultPromises);
+  try {
+    await Promise.all(resultPromises);
+  } catch(e) {
+    logger.warn("Callback error:", e);
+  }
   logger.verbose('sync finished.');
 
   return completedResults;
 }
-
