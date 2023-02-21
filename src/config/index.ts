be776141d4b67dfb83de7a88e0ed18bbb754fb39
Takadenoshi <tasos@kadena.io>
2023-02-21 18:27:57 +0100

    config cleanup

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,23 +1,11 @@
 import 'dotenv/config';
 
-/*
- * Helpers
- */
-function die(message) {
-  console.error(`FATAL: ${message}`);
-  process.exit(1);
-}
-
-function isProduction(NODE_ENV: string) {
-  return NODE_ENV === 'production';
-}
-
 type ConfigVars = 'network' | 'dataHost' | 'chainwebHost' |
   'port' | 'redisHost' | 'redisPassword' | 'confirmationHeight' |
   'eventsStepInterval' | 'chainwebCutUpdateInterval' | 'httpMaxRetries' | 'httpRetryBackoffStep' |
   'log' | 'production' | 'moduleHashBlacklist';
 
-type ConfigSpecification = {
+interface ConfigSpecification {
   varName: ConfigVars;
   envName: string;
   numeric?: boolean;
@@ -178,72 +166,13 @@ export function generateConfig(env: NodeJS.ProcessEnv, configSpec: ConfigSpecifi
 }
 
 /*
-interface Config {
-  port: number;
-  dataHost: string;
-  chainwebHost: string;
-  network: string;
-  moduleHashBlacklist: string[];
-  log: string;
-  confirmationHeight: number;
-  eventsStepInterval: number;
-  redisHost: string;
-  redisPassword: string;
+ * Helpers
+ */
+function die(message) {
+  console.error(`FATAL: ${message}`);
+  process.exit(1);
 }
 
-/*
- * Default config includes module hash blacklist for now
-export const config: Config = generateConfig();
-
-function generateConfig(): Config {
-  const config = {
-    moduleHashBlacklist: [
-      'LKQj2snGFz7Y8iyYlSm3uIomEAYb0C9zXCkTIPtzkPU',
-      'F7tD1QlT8dx8BGyyq-h22OECYS7C3FfcYaRyxt6D1YQ',
-      'WSIFGtnAlLCHFcFEHaKGrGeAG4qnTsZRj9BdvzzGa6w',
-      '4m9KUKUzbd9hVZoN9uIlJkxYaf1NTz9G7Pc9C9rKTo4',
-      '_1rbpI8gnHqflwb-XqHsYEFBCrLNncLplikh9XFG-y8',
-      'dhIGiZIWED2Rk6zIrJxG8DeQn8n7WDKg2b5cZD2w4CU',
-      'cOJgr8s3j3p5Vk0AAqjdf1TzvWZlFsAiq4pMiOzUo1w',
-      'HsePyFCyYUPEPJqG5VymbQkkI3gsPAQn218uWEF_dbs',
-      'lWqEvH5U20apKfBn27HOBaW46vQlxhkiDtYHZ5KoYp0',
-      'uvtUnp96w2KnxnneYa4kUN1kTvYSa8Ma33UDkQXV0NA',
-      '78ngDzxXE8ZyHE-kFm2h7-6Xm8N8uwU_xd1fasO8gWU',
-    ],
-  };
-
-  // map required keys
-  for (const [requireKey, configKey] of Object.entries(REQUIRE_KEYS)) {
-    const envValue = process.env[requireKey];
-    if (typeof envValue === "undefined") {
-      die(`Required env var missing: ${requireKey}\nCopy .default.env to .env and set up the variables to your preference.`);
-    }
-    config[configKey] = envValue;
-  }
-
-  // map keys with default values
-  for (const [envKey, [configKey, defaultValue]] of Object.entries(DEFAULT_KEYS)) {
-    const envValue = process.env[envKey];
-    config[configKey] = envValue ?? defaultValue;
-  }
-
-  // cast & enforce numeric keys
-  for (const key of NUMERIC_KEYS) {
-    const value = config[key];
-    const intValue = parseInt(value, 10);
-    if (!isNaN(intValue)) {
-      config[key] = intValue;
-      continue;
-    }
-    const floatValue = parseFloat(value);
-    if (!isNaN(floatValue)) {
-      config[key] = floatValue;
-      continue;
-    }
-    die(`Numeric env var not parsable as number: ${key} value: ${value}`);
-  }
-
-  return config;
+function isProduction(NODE_ENV: string) {
+  return NODE_ENV === 'production';
 }
-
-*/
