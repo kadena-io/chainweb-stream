0204386a95c2aefe67c5ef186be3aa496219f857
Takadenoshi <tasos@kadena.io>
2023-07-20 17:52:17 +0300

    prettier

--- src/sse/chainweb-event-state.ts
+++ src/sse/chainweb-event-state.ts
@@ -1,13 +1,14 @@
+import Logger from '../logger.js';
+
 import {
-  getRedisUnconfirmedEvents,
   getRedisConfirmedEvents,
   getRedisOrphanedEvents,
-  setRedisUnconfirmedEvents,
+  getRedisUnconfirmedEvents,
   setRedisConfirmedEvents,
   setRedisOrphanedEvents,
+  setRedisUnconfirmedEvents,
 } from './redis/index.js';
-import { TransactionType, GenericData } from './types.js';
-import Logger from '../logger.js';
+import { GenericData, TransactionType } from './types.js';
 
 type DataFilterPredicate = (data: GenericData) => boolean;
 
@@ -18,7 +19,7 @@ interface GetEventOptions {
 }
 
 interface PresentEventOptions extends GetEventOptions {
-  sort?: boolean
+  sort?: boolean;
 }
 
 function heightSorter(a: GenericData, b: GenericData) {
@@ -28,22 +29,23 @@ function heightSorter(a: GenericData, b: GenericData) {
 
 function presentEvents(sources: GenericData[][], options: PresentEventOptions): GenericData[] {
   const { minHeight, maxHeight, limit } = options;
-  if (limit === 0) { // early return
+  if (limit === 0) {
+    // early return
     return [];
   }
   const filterPredicates: DataFilterPredicate[] = [];
   if (minHeight !== undefined) {
-    filterPredicates.push(event => event.height >= minHeight);
+    filterPredicates.push((event) => event.height >= minHeight);
   }
   if (maxHeight !== undefined) {
-    filterPredicates.push(event => event.height <= maxHeight);
+    filterPredicates.push((event) => event.height <= maxHeight);
   }
 
   const output = sources.reduce((out, arr) => {
     if (filterPredicates.length) {
       // TODO if we have a limit set and a lot of data
       // we can save a bunch by returning up to limit from each arr
-      arr = arr.filter(event => filterPredicates.every(pred => pred(event)));
+      arr = arr.filter((event) => filterPredicates.every((pred) => pred(event)));
     }
     return out.concat(arr);
   }, []);
@@ -59,9 +61,9 @@ export default class ChainwebEventServiceState {
   _filter: string;
   logger: Logger;
 
-  unconfirmed: GenericData[] = []
-  confirmed: GenericData[] = []
-  orphaned: GenericData[] = []
+  unconfirmed: GenericData[] = [];
+  confirmed: GenericData[] = [];
+  orphaned: GenericData[] = [];
 
   constructor({ type, filter, logger }) {
     this._type = type;
@@ -70,11 +72,7 @@ export default class ChainwebEventServiceState {
   }
 
   async load(): Promise<void> {
-    const [
-      unconfirmed,
-      confirmed,
-      orphaned,
-    ] = await Promise.all([
+    const [unconfirmed, confirmed, orphaned] = await Promise.all([
       getRedisUnconfirmedEvents(this._type, this._filter),
       getRedisConfirmedEvents(this._type, this._filter),
       getRedisOrphanedEvents(this._type, this._filter),
@@ -97,30 +95,26 @@ export default class ChainwebEventServiceState {
   }
 
   getAllEvents({ minHeight, maxHeight, limit }: GetEventOptions = {}): GenericData[] {
-    return presentEvents(
-      [this.unconfirmed, this.confirmed, this.orphaned],
-      { minHeight, maxHeight, limit, sort: true },
-    );
+    return presentEvents([this.unconfirmed, this.confirmed, this.orphaned], {
+      minHeight,
+      maxHeight,
+      limit,
+      sort: true,
+    });
   }
 
   getConfirmedEvents({ minHeight, maxHeight, limit }: GetEventOptions = {}): GenericData[] {
-    return presentEvents(
-      [this.confirmed],
-      { minHeight, maxHeight, limit, sort: false },
-    );
+    return presentEvents([this.confirmed], { minHeight, maxHeight, limit, sort: false });
   }
 
   getOrphanedEvents({ minHeight, maxHeight, limit }: GetEventOptions = {}): GenericData[] {
-    return presentEvents(
-      [this.orphaned],
-      { minHeight, maxHeight, limit, sort: false },
-    );
+    return presentEvents([this.orphaned], { minHeight, maxHeight, limit, sort: false });
   }
 
   /* add deduped and sorted */
   add(permanence, event): boolean {
     const { height } = event;
-    for(let idx = 0; idx < this[permanence].length; idx++) {
+    for (let idx = 0; idx < this[permanence].length; idx++) {
       const existing = this[permanence][idx];
       if (existing.height > height) {
         continue;
@@ -136,25 +130,37 @@ export default class ChainwebEventServiceState {
       }
     }
     // fallback; no existing events OR pushing oldest event
-    this[permanence].push(event)
+    this[permanence].push(event);
     return true;
   }
 
   remove(permanence, event): boolean {
     const idx = this.unconfirmed.indexOf(event);
     if (idx === -1) {
-      this.logger.warn(`Could not find event ${event.name} ${event.requestKey} from ${permanence} while trying to remove it from ${permanence}`);
+      this.logger.warn(
+        `Could not find event ${event.name} ${event.requestKey} from ${permanence} while trying to remove it from ${permanence}`,
+      );
       return false;
     }
     this[permanence].splice(idx, 1);
     return true;
   }
 
-  _eventExists(needle: GenericData, collection: GenericData[], startIdx=0): boolean {
-    const { height, requestKey, blockHash, meta: { id } } = needle;
-    for(let idx = startIdx; idx < collection.length; idx++) {
+  _eventExists(needle: GenericData, collection: GenericData[], startIdx = 0): boolean {
+    const {
+      height,
+      requestKey,
+      blockHash,
+      meta: { id },
+    } = needle;
+    for (let idx = startIdx; idx < collection.length; idx++) {
       const event = collection[idx];
-      if (event.meta.id !== id || event.height !== height || event.requestKey !== requestKey || event.blockHash !== blockHash) {
+      if (
+        event.meta.id !== id ||
+        event.height !== height ||
+        event.requestKey !== requestKey ||
+        event.blockHash !== blockHash
+      ) {
         // return early if basic stuff is different - save us a JSON.stringify
         continue;
       }
@@ -169,5 +175,4 @@ export default class ChainwebEventServiceState {
     }
     return false;
   }
-
 }
